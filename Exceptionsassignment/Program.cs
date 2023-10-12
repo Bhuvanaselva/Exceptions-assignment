3c8547b42c97d5c13bc5c5ff01200a27ffeb74bc
¨Bhuvana¨ <bhuvanaselvaraj25@gmail.com>
2023-10-12 14:51:13 +0200

    Add project files.

---
+++ Exceptionsassignment/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using Exceptionsassignment;
+using System.IO;
+
+//step 1
+
+string input = "10,20,30,40,abc,50,60,xyz";
+Console.WriteLine($"\nString: {input}");
+
+List<int> result = WordsDTO.GetNumbersInDescending(input);
+
+Console.WriteLine("\nNumbers in descending order:");
+   
+foreach (int number in result)
+{
+Console.WriteLine(number);
+}
+
+
+//Step 3   
+
+string filePath = "C:\\Users\\bhuva\\source\\repos\\Exceptionsassignment\\Exceptionsassignment\\sample.txt";
+WordsDTO wordsDTO = WordsDTO.ReadFileContent(filePath);
+Console.WriteLine("\nWordsDTO!");
+Console.WriteLine("\nString Array:\n\n" + string.Join(", ", wordsDTO.StringArray));
+Console.WriteLine("\nBool: " + wordsDTO.BoolValue);
+Console.WriteLine("\nString: " + wordsDTO.StringValue);
+
+if (wordsDTO.BoolValue!= null)
+{
+Console.WriteLine("\nWords read from the file:\n");
+
+   foreach (string word in wordsDTO.StringArray)
+    {
+    Console.WriteLine(word);
+    }
+}
+else
+{
+Console.WriteLine("Error: " + wordsDTO.StringValue);
+}
+
+
+
---
+++ Exceptionsassignment/WordsDTO.cs
@@ -0,0 +1,64 @@
+﻿using Exceptionsassignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptionsassignment
+{
+    internal class WordsDTO
+    {
+        // step 2
+        public string[] StringArray { get; set; }
+        public bool BoolValue{ get; set; }
+        public string StringValue { get; set; }
+    
+        //step 1
+    public static List<int> GetNumbersInDescending(string input)
+    {
+        string[] numberStrings = input.Split(',');
+        List<int> numbers = new List<int>();
+
+        foreach (string numberString in numberStrings)
+        {
+            try
+            {
+                int number = int.Parse(numberString);
+                numbers.Add(number);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("\nEntry discarded: " + numberString);
+            }
+        }
+            numbers.Sort((a, b) => b.CompareTo(a));
+            return numbers;
+    }
+
+        //step 3
+    public static WordsDTO ReadFileContent(string filePath)
+    {
+        WordsDTO dto = new WordsDTO();
+
+        try
+        {
+            string fileContents = File.ReadAllText(filePath);
+            dto.StringArray = fileContents.Split(',');
+            dto.BoolValue = true;
+            dto.StringValue = "Success";
+        }
+        catch (FileNotFoundException)
+        {
+            dto.BoolValue = false;
+            dto.StringValue = "File not found";
+        }
+        catch (Exception ex)
+            {
+             Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            return dto;
+    }
+
+}
+}
